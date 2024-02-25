bfc5ffe4891e55b33359ed24d5a5ef2b41dca85e
xsware <xsware@outlook.com>
2024-02-25 15:42:08 +0100

    rework

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(allocator_api)]
 #![feature(slice_ptr_get)]
+#![feature(non_null_convenience)]
 
 use std::{
     alloc::{AllocError, Allocator, Layout},
-    cell::UnsafeCell,
     ptr::NonNull,
     sync::{
         atomic::{AtomicUsize, Ordering},
@@ -12,7 +12,7 @@ use std::{
 };
 
 pub struct Arena {
-    mem_pool: Box<[u8]>,
+    mem_pool: NonNull<[u8]>,
     offset: AtomicUsize,
 }
 
@@ -22,39 +22,21 @@ unsafe impl Sync for Arena {}
 impl Arena {
     pub fn new(capacity: usize) -> Self {
         Self {
-            mem_pool: vec![0; capacity].into_boxed_slice(),
+            mem_pool: unsafe { NonNull::new_unchecked(Box::into_raw(vec![0; capacity].into_boxed_slice())) },
             offset: AtomicUsize::new(0),
         }
     }
-}
-
-#[derive(Clone)]
-pub struct ArenaAllocator {
-    arena: Arc<Arena>,
-}
-
-impl ArenaAllocator {
-    pub fn new(arena: Arena) -> Self {
-        Self { arena: Arc::new(arena) }
-    }
-
-    fn get_mem_pool(&self) -> &UnsafeCell<[u8]> {
-        unsafe { std::mem::transmute(self.arena.mem_pool.as_ref()) }
-    }
 
     fn capacity(&self) -> usize {
-        self.arena.mem_pool.len()
+        self.mem_pool.len()
     }
 
-    #[allow(unused)]
-    fn print_arena(&self) {
-        unsafe {
-            println!("{:?}", &self.arena.mem_pool);
-        }
+    fn get_next_mem_slice(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        let (start, end) = self.get_aligned_memory_bounds(layout)?;
+        unsafe { Ok(self.mem_pool.get_unchecked_mut(start..end)) }
     }
 
     /// returns the offset start and end for the allocated memory.
-    #[must_use]
     fn get_aligned_memory_bounds(&self, layout: Layout) -> Result<(usize, usize), AllocError> {
         let requested_size = layout.size();
 
@@ -62,8 +44,8 @@ impl ArenaAllocator {
             // get a copy of the current value of the offset.
             // it is important that the value used for the calculations can't be changed by another thread
             // since we need to very the result with a compare_exchange in the end.
-            let offset = self.arena.offset.load(Ordering::Acquire);
-            let align_offset = unsafe { self.get_mem_pool().get().as_mut_ptr().add(offset).align_offset(layout.align()) };
+            let offset = self.offset.load(Ordering::Acquire);
+            let align_offset = unsafe { self.mem_pool.as_mut_ptr().add(offset).align_offset(layout.align()) };
 
             // failed to find suitable alignment
             if align_offset == usize::MAX {
@@ -82,7 +64,6 @@ impl ArenaAllocator {
             // if there is enough space available and nobody else claimed that space in the meantime the result is returned.
             // otherwise the process needs to be retried.
             if self
-                .arena
                 .offset
                 .compare_exchange(offset, end, Ordering::Release, Ordering::SeqCst)
                 .is_ok()
@@ -93,14 +74,35 @@ impl ArenaAllocator {
     }
 }
 
+impl Drop for Arena {
+    fn drop(&mut self) {
+        unsafe {
+            let _: Box<_> = Box::from_raw(self.mem_pool.as_mut());
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ArenaAllocator {
+    arena: Arc<Arena>,
+}
+
+impl ArenaAllocator {
+    pub fn new(arena: Arena) -> Self {
+        Self { arena: Arc::new(arena) }
+    }
+
+    #[allow(unused)]
+    fn print_arena(&self) {
+        unsafe {
+            println!("{:?}", &self.arena.mem_pool);
+        }
+    }
+}
+
 unsafe impl Allocator for ArenaAllocator {
     fn allocate(&self, layout: Layout) -> Result<std::ptr::NonNull<[u8]>, AllocError> {
-        let (mem_start, mem_end) = self.get_aligned_memory_bounds(layout)?;
-
-        unsafe {
-            let mem_ptr = self.get_mem_pool().get().get_unchecked_mut(mem_start..mem_end);
-            Ok(NonNull::<[u8]>::new_unchecked(mem_ptr))
-        }
+        self.arena.get_next_mem_slice(layout)
     }
 
     unsafe fn deallocate(&self, _ptr: std::ptr::NonNull<u8>, _layout: Layout) {}
